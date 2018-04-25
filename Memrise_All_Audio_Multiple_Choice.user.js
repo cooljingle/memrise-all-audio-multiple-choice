206fced661de79925c426bd7949f57a905c2ae5e
Aaron Miles <cool_jingle@hotmail.co.uk>
2018-04-25 18:28:28 +0100

    fix after session_start was removed

--- Memrise_All_Audio_Multiple_Choice.user.js
+++ Memrise_All_Audio_Multiple_Choice.user.js
@@ -4,20 +4,17 @@
 // @description    Turns all tests into audio multiple choice
 // @match          https://www.memrise.com/course/*/garden/*
 // @match          https://www.memrise.com/garden/review/*
-// @version        0.0.3
+// @version        0.0.4
 // @updateURL      https://github.com/cooljingle/memrise-all-audio-multiple-choice/raw/master/Memrise_All_Audio_Multiple_Choice.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-all-audio-multiple-choice/raw/master/Memrise_All_Audio_Multiple_Choice.user.js
 // @grant          none
 // ==/UserScript==
 
 $(document).ready(function() {
-    MEMRISE.garden.session_start = (function() {
-        var cached_function = MEMRISE.garden.session_start;
-        return function() {
-            enableAllAudioMultipleChoice();
-            return cached_function.apply(this, arguments);
-        };
-    }());
+
+    MEMRISE.garden._events.start.push(() => {
+        enableAllAudioMultipleChoice();
+    });
 
     function enableAllAudioMultipleChoice() {
         MEMRISE.garden.session.box_factory.make = (function() {
